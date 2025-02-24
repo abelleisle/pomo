868b1a0d9e63fc741393cd794a20508cc4dbf54b
abelleisle <abelleisle@protonmail.com>
2025-02-23 19:50:10 -0500

    Got large text rendering :)

--- src/font.zig
+++ src/font.zig
@@ -1,17 +1,83 @@
+const std = @import("std");
+
+const String = @import("String.zig");
+
 fn font(font_height: u32) type {
     return struct {
         const Font = @This();
         const height = font_height;
 
         numbers: [10][height][]const u8,
-        semicolon: [height][]const u8,
+        colon: [height][]const u8,
         unknown: [height][]const u8,
+        separator: []const u8,
+
+        pub fn get_height(self: *const Font) usize {
+            _ = self;
+            return font_height;
+        }
+
+        pub fn get_width(self: *const Font, str: []const u8) usize {
+            var width: usize = 0;
+            for (str) |s| {
+                switch (s) {
+                    '0'...'9' => {
+                        const digit = s - '0';
+                        if (digit <= 10) {
+                            width += self.numbers[digit][0].len - 1;
+                        }
+                    },
+                    ':' => width += self.colon.len - 1,
+                    else => width += self.unknown.len - 1,
+                }
+
+                width += self.separator.len;
+            }
+
+            width -= self.separator.len;
+
+            return width;
+        }
+
+        pub fn get_str(self: *const Font, allocator: std.mem.Allocator, str: []const u8) !String {
+            const width = self.get_width(str) + 1; // Add 1 for newlines
+            const total = width * height;
+
+            var string = try String.initCapacity(allocator, total);
+
+            for (0..height) |i| {
+                var s_index: usize = 0;
+                for (str) |s| {
+                    switch (s) {
+                        '0'...'9' => {
+                            const digit = s - '0';
+                            if (digit <= 10) {
+                                const ss = self.numbers[digit][i];
+                                try string.append(ss);
+                            }
+                        },
+                        ':' => try string.append(self.colon[i]),
+                        else => try string.append(self.unknown[i]),
+                    }
+
+                    s_index += 1;
+                    if (s_index < str.len) {
+                        try string.append(self.separator);
+                    }
+                }
+
+                try string.append("\n");
+            }
+
+            return string;
+        }
     };
 }
 
 const Small = font(1);
-const Normal = font(9);
+const Normal = font(5);
 
+// zig fmt: off
 pub const small = Small{
     .numbers = .{
         .{"0"},
@@ -25,6 +91,87 @@ pub const small = Small{
         .{"8"},
         .{"9"},
     },
-    .semicolon = .{":"},
+    .colon = .{":"},
     .unknown = .{"?"},
+    .separator = "",
 };
+
+pub const medium = Normal{
+    .numbers = .{
+        .{"███",
+          "█ █",
+          "█ █",
+          "█ █",
+          "███",
+        },
+        .{"██▏", // TODO look at why I need this 1/8 block here.
+          " █ ", // Without it, the .len becomes 2???
+          " █ ",
+          " █ ",
+          "███",
+        },
+        .{"███",
+          "  █",
+          "███",
+          "█  ",
+          "███",
+        },
+        .{"███",
+          "  █",
+          "███",
+          "  █",
+          "███",
+        },
+        .{"█▕█", // TODO similar to 1, this reports .len = 2 if
+          "█ █", // top isn't 3 non-space chars
+          "███",
+          "  █",
+          "  █",
+        },
+        .{"███",
+          "█  ",
+          "███",
+          "  █",
+          "███",
+        },
+        .{"███",
+          "█  ",
+          "███",
+          "█ █",
+          "███",
+        },
+        .{"███",
+          "  █",
+          "  █",
+          "  █",
+          "  █",
+        },
+        .{"███",
+          "█ █",
+          "███",
+          "█ █",
+          "███",
+        },
+        .{"███",
+          "█ █",
+          "███",
+          "  █",
+          "███",
+        },
+    },
+    .colon = .{" ",
+               "█",
+               " ",
+               "█",
+               " ",
+    },
+    .unknown = .{"███",
+                 "█ █",
+                 " ██",
+                 " █ ",
+                 " █ ",
+
+    },
+    .separator = " ",
+};
+// zig fmt: on
--- src/main.zig
+++ src/main.zig
@@ -3,170 +3,8 @@ const std = @import("std");
 const vaxis = @import("vaxis");
 const vxfw = vaxis.vxfw;
 
+const Timer = @import("Timer.zig");
 const font = @import("font.zig");
-
-const Timer = struct {
-    const MAX = (99 * std.time.s_per_hour) + (59 * std.time.s_per_min) + 59;
-    duration: i64,
-    remaining: i64,
-    expires: i64,
-    buf: [9]u8,
-
-    // These let us remember the length of a timer without changing the width
-    hours: ?u64,
-    minutes: ?u64,
-    seconds: u64,
-
-    running: bool,
-    started: ?i64,
-    force_redraw: bool,
-
-    pub fn init(duration_sec: i64) !Timer {
-        if (duration_sec > Timer.MAX) return error.TooLarge;
-        const now = std.time.milliTimestamp();
-        const duration_ms = (duration_sec * std.time.ms_per_s);
-        const expires = now + duration_ms;
-
-        // zig fmt: off
-        var timer = Timer{
-            .duration = duration_ms,
-            .remaining = duration_ms,
-            .expires = expires,
-            .buf = std.mem.zeroes([9]u8),
-
-            .hours = if (duration_sec >= std.time.s_per_hour) 0 else null,
-            .minutes = if (duration_sec >= std.time.s_per_min) 0 else null,
-            .seconds = @intCast(duration_sec),
-
-            .running = false,
-            .started = null,
-            .force_redraw = false,
-        };
-        // zig fmt: on
-
-        _ = timer.update_digits();
-
-        return timer;
-    }
-
-    pub fn start(self: *Timer) void {
-        if (!self.running and !self.expired()) {
-            self.started = std.time.milliTimestamp();
-            self.running = true;
-            self.force_redraw = true;
-        }
-    }
-
-    pub fn pause(self: *Timer) void {
-        self.running = false;
-        self.force_redraw = true;
-    }
-
-    pub fn expired(self: *const Timer) bool {
-        return self.expires <= std.time.milliTimestamp();
-    }
-
-    pub fn reset(self: *Timer) void {
-        self.running = false;
-        self.remaining = self.duration;
-        const now = std.time.milliTimestamp();
-        self.expires = now + self.duration;
-        self.force_redraw = true;
-    }
-
-    pub fn update(self: *Timer) bool {
-        if (!self.running) {
-            if (self.force_redraw) {
-                return self.update_digits();
-            }
-            return false;
-        }
-
-        if (self.started) |started| {
-            self.expires = started + self.remaining;
-            self.started = null;
-        }
-
-        const now = std.time.milliTimestamp();
-
-        self.remaining = self.expires - now;
-        if (self.remaining < 0) {
-            self.remaining = 0;
-            self.running = false;
-        }
-
-        return self.update_digits();
-    }
-
-    fn update_digits(self: *Timer) bool {
-        self.force_redraw = false;
-
-        var updated = false;
-
-        var dur: u64 = @intCast(self.remaining);
-
-        // If we're tracking hours, update the hour counter
-        if (self.hours) |*h| {
-            const hours = @divTrunc(dur, std.time.ms_per_hour);
-            dur = @rem(dur, std.time.ms_per_hour);
-
-            if (h.* != hours) {
-                h.* = hours;
-                updated = true;
-            }
-        }
-
-        // If we're tracking minutes, update the minute counter
-        if (self.minutes) |*m| {
-            const minutes = @divTrunc(dur, std.time.ms_per_min);
-            dur = @rem(dur, std.time.ms_per_min);
-            if (m.* != minutes) {
-                m.* = minutes;
-                updated = true;
-            }
-        }
-
-        const seconds = @divTrunc(dur, std.time.ms_per_s);
-        if (self.seconds != seconds) {
-            self.seconds = seconds;
-            updated = true;
-        }
-
-        return updated;
-    }
-
-    pub fn display(self: *Timer) ![]const u8 {
-        if (self.hours) |hours| {
-            if (self.minutes) |minutes| {
-                // zig fmt: off
-                return try std.fmt.bufPrint(
-                    &self.buf,
-                    "{d:0>2}:{d:0>2}:{d:0>2}",
-                    .{ hours, minutes, self.seconds }
-                );
-                // zig fmt: on
-            } else {
-                @panic("Having hours but no minutes is not possible");
-            }
-        } else if (self.minutes) |minutes| {
-            // zig fmt: off
-            return try std.fmt.bufPrint(
-                &self.buf,
-                "{d:0>2}:{d:0>2}",
-                .{ minutes, self.seconds }
-            );
-            // zig fmt: on
-        } else {
-            // zig fmt: off
-            return try std.fmt.bufPrint(
-                &self.buf,
-                "{d:0>2}",
-                .{ self.seconds }
-            );
-            // zig fmt: on
-        }
-    }
-};
 
 // This can contain internal events as well as Vaxis events.
 // Internal events can be posted into the same queue as vaxis events to allow
@@ -260,23 +98,33 @@ const App = struct {
 
         // Create a bordered child window
         const child = win.child(.{
-            .x_off = win.width / 2 - 20,
-            .y_off = win.height / 2 - 12,
-            .width = 40,
-            .height = 24,
+            .x_off = win.width / 2 - 40,
+            .y_off = win.height / 2 - 22,
+            .width = 80,
+            .height = 44,
             .border = .{
                 .where = .all,
                 .style = style,
             },
         });
 
+        const displayed = try self.timer.display();
+        const wh: u16 = @intCast(font.medium.get_width(displayed));
+        const fh: u16 = @intCast(font.medium.get_height());
+
+        const whh = @max(1, wh / 2);
+        // const fhh = @max(1, fh / 2);
+
+        var str = try font.medium.get_str(self.allocator, displayed);
+        defer str.deinit();
+
         // Create the countdown clock text
         const segment: vaxis.Segment = .{
-            .text = try self.timer.display(),
+            .text = str.str(),
             .style = style,
         };
         // Center the countdown clock text
-        const center = vaxis.widgets.alignment.center(child, 28, 4);
+        const center = vaxis.widgets.alignment.center(child, whh, fh);
         _ = center.printSegment(segment, .{ .wrap = .grapheme });
 
         try self.vx.render(self.tty.anyWriter());
@@ -289,7 +137,7 @@ pub fn main() !void {
 
     const allocator = gpa.allocator();
 
-    var app = try App.init(allocator, 5);
+    var app = try App.init(allocator, 1500);
     defer app.deinit();
 
     try app.run();
